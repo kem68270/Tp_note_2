d30a421948aa0e3f16d99de2048c0669d2c217d1
Jeremy Yang <jeremy.yang@etu.univ-tours.fr>
2024-12-13 08:40:36 +0100

    Filmbean

---
+++ src/main/java/fr/univtours/polytech/tp_note_2/model/FilmBean.java
@@ -0,0 +1,39 @@
+package fr.univtours.polytech.tp_note_2.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class FilmBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nom;
+    private Integer note;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public Integer getNote() {
+        return note;
+    }
+    public void setNote(Integer note) {
+        this.note = note;
+    }
+
+}
